7af02a9fd3911a76cfac53577b5932e27b69538c
vincent <ikbengerrit92@gmail.com>
2021-10-13 14:54:03 +0200

    :bug: carousel to be fixed

--- transformers-web-app/resources/views/welcome.blade.php
+++ transformers-web-app/resources/views/welcome.blade.php
@@ -43,7 +43,7 @@
                     @if (Route::has('login'))
                         <div class="d-flex align-items-center">
                             @auth
-                                <a href="{{ url('/dashboard') }}" class="">Dashboard</a>
+                                <a href="{{ url('/dashboard') }}" class="btn c-titles-font c-imp-button m-2">Dashboard</a>
                             @else
                                 @if (Route::has('register'))
                                     <a href="{{ route('register') }}" class="btn c-titles-font c-link-cw">Register</a>
@@ -66,16 +66,47 @@
                     <h3>Maar je staat er niet alleen voor.</h3>
                 </span>
                 <p class="c-text-font">
-                    De Transformers Community is er voor jongeren die zelfverzekerd willen zijn en tegenslagen omzetten in <br> kracht. We doen dit samen: zo leren we meer en helpen we elkaar om te groeien.
+                    De Transformers Community is er voor jongeren die zelfverzekerd willen zijn <br> en tegenslagen omzetten in kracht. <br> We doen dit samen: zo leren we meer en helpen we elkaar om te groeien.
                 </p>
                 <a href="{{ route('login') }}" class="btn c-titles-font c-imp-button mt-10 pt-2 pb-2 ps-5 pe-5">Naar community</a>
+            </div>
+
+            <div id="carouselExampleCaptions" class="carousel slide d-sm-none d-md-block ps-2 pe-2" data-bs-ride="carousel">
+              <div class="carousel-indicators">
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+              </div>
+              <div class="carousel-inner">
+                <div class="carousel-item active">
+                  <img src="https://static.wixstatic.com/media/e72157_db107074d02f49ee85c5460d7358244e~mv2.png/v1/fill/w_796,h_450,al_c,q_85,usm_0.66_1.00_0.01/e72157_db107074d02f49ee85c5460d7358244e~mv2.webp" class="d-block mx-auto w-auto" alt="...">
+                  <div class="carousel-caption d-none d-md-block">
+                    <h5>Lisa (24 jaar)</h5>
+                    <p>Some representative placeholder content for the first slide.</p>
+                  </div>
+                </div>
+                <div class="carousel-item">
+                  <img src="https://static.wixstatic.com/media/e72157_eaee7f30427e47e0893180b6a2cac833~mv2.png/v1/fill/w_796,h_450,al_c,q_85,usm_0.66_1.00_0.01/e72157_eaee7f30427e47e0893180b6a2cac833~mv2.webp" class="d-block mx-auto w-auto" alt="...">
+                  <div class="carousel-caption d-none d-md-block">
+                    <h5>Elwin (29 jaar)</h5>
+                    <p>Some representative placeholder content for the second slide.</p>
+                  </div>
+                </div>
+                <div class="carousel-item">
+                  <img src="https://static.wixstatic.com/media/e72157_c01472345d2b461381e28a781dc86d9f~mv2.png/v1/fill/w_796,h_450,al_c,q_85,usm_0.66_1.00_0.01/e72157_c01472345d2b461381e28a781dc86d9f~mv2.webp" class="d-block mx-auto w-auto" alt="...">
+                  <div class="carousel-caption d-none d-md-block">
+                    <h5>Yasmine (22 jaar)</h5>
+                    <p>Some representative placeholder content for the third slide.</p>
+                  </div>
+                </div>
+              </div>
             </div>
         </div>
         <img src="./images/wave1.svg">
 
 
         {{--  --}}
-        <div class="c-tb text-center pb-5">
+        <div class="c-tb text-center pb-2 pt-2">
           <h2 class="c-titles-font fw-bold">Wij zijn een groeiende beweging van jongeren die zich <br> inzet voor <span class="c-underline">mentale gezondheid.</span></h2>
           <br>
           <p>
@@ -86,5 +117,8 @@
           </p>
           <a href="#}" class="btn c-titles-font c-imp-button mt-10 pt-2 pb-2 ps-5 pe-5">Onze missie en visie</a>
         </div>
+
+        {{-- --}}
+        <img src="./images/wave2.svg">
     </body>
 </html>
