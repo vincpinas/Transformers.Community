5c3e5937d6d9d2ff473429b2b46cc064881ba966
vincent <ikbengerrit92@gmail.com>
2021-11-10 10:45:50 +0100

    :bug: local -v bug fixes

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -17,49 +17,54 @@
         </style>
     </head>
     <body class="antialiased">
-        <nav class="navbar navbar-expand-lg navbar-light c-bg-purple c-tw">
-            <div class="container-fluid">
-              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-              </button>
-              <a class="navbar-brand" href="#">
-                <img src="{{ asset("/images/logo.svg") }}" class="c-logo-80 d-none d-lg-block">
-              </a>
-              <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                  <li class="nav-item">
-                    <a class="nav-link c-tw"  href="/">Home</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link c-tw" href="/feed">Feed</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link c-tw" href="#">Over Ons</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link c-tw" href="#">Word Transformer!</a>
-                  </li>
-                  <li class="nav-item">
-                    <a class="nav-link c-tw" href="#">Contact</a>
-                  </li>
-                </ul>
-                <div class="d-flex justify-content-end align-items-center">
-                    @if (Route::has('login'))
-                        <div class="d-flex align-items-center">
-                            @auth
-                                <a href="{{ url('/dashboard') }}" class="btn c-titles-font c-imp-button m-2">Dashboard</a>
-                            @else
-                                @if (Route::has('register'))
-                                    <a href="{{ route('register') }}" class="btn c-titles-font c-link-cw">Register</a>
-                                @endif
-                                    <a href="{{ route('login') }}" class="btn c-titles-font c-imp-button m-2">Login</a>
-                            @endauth
-                        </div>
-                    @endif
-                </div>
-              </div>
+      <nav class="navbar navbar-expand-lg navbar-light c-bg-purple c-tw">
+        <div class="container-fluid">
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <a class="navbar-brand" href="#">
+            <img src="{{ asset("images/logo.svg") }}" class="c-logo-80 d-none d-lg-block">
+          </a>
+          <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+              <li class="nav-item">
+                <a class="nav-link c-tw"  href="/">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link c-tw" href="/feed">Feed</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link c-tw" href="#">Over Ons</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link c-tw" href="#">Word Transformer!</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link c-tw" href="#">Contact</a>
+              </li>
+            </ul>
+            <div class="d-flex justify-content-end align-items-center">
+                @if (Route::has('login'))
+                    <div class="d-flex align-items-center">
+                        @auth
+                          <div>
+                            <form method="POST" action="{{ route('logout') }}">
+                                @csrf
+                                <button class="btn c-titles-font c-imp-button">{{ __('Log Out') }}</button>
+                            </form>
+                          </div>
+                        @else
+                            @if (Route::has('register'))
+                                <a href="{{ route('register') }}" class="btn c-titles-font c-link-cw">Register</a>
+                            @endif
+                                <a href="{{ route('login') }}" class="btn c-titles-font c-imp-button m-2">Login</a>
+                        @endauth
+                    </div>
+                @endif
             </div>
-          </nav>
+          </div>
+        </div>
+      </nav>
 
         <article>
             <h2>{!! $post->title !!}</h2>
